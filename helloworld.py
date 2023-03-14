0ba00810c0ff6c1150f579cfd4b3c39713373b0c
pratiek730 <prateek.parashar730@gmail.com>
2023-03-14 21:33:39 +0530

    First ever commit

---
+++ helloworld.py
@@ -0,0 +1,5 @@
+x = int(input("Enter Kilometers "))
+y = int(input("Enter Meters "))
+z = int(input("Enter Centimeters "))
+total = (x*100000) + (y*100) + z
+print("Total Distance in Centimeters =",total)
